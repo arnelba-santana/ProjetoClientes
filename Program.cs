20609273a80066168e53342db157a30a2d7c0526
arnelba.santana <rochaprotector@gmail.com>
2023-12-21 23:31:09 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.SqlClient;
+
+namespace ProjetoClientes;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        try
+        {
+
+            //executar
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(
+    "User Id=sa;Password=656913;" +
+    "Server=localhost\\SQLEXPRESS;" +
+    "Database=projetoclientes;" +
+    "Trusted_Connection=False;"
+);
+
+            using (SqlConnection conexao = new SqlConnection(builder.ConnectionString))
+            {
+
+                string sql = " Select * from clientes ";
+                using (SqlCommand comando = new SqlCommand(sql, conexao))
+                {
+                    conexao.Open();
+                    using (SqlDataReader leitor = comando.ExecuteReader())
+                    {
+                        while (leitor.Read())
+                        {
+                            System.Console.WriteLine("id: {0}", leitor.GetSqlInt32(0));
+                            System.Console.WriteLine("nome: {0}", leitor.GetSqlString(1));
+                            System.Console.WriteLine("email: {0}", leitor.GetSqlString(2));
+
+                        }
+                    }
+                }
+            }
+        }
+
+        catch (Exception e)
+        {
+            //exception
+            System.Console.WriteLine("Erro:" + e.ToString());
+        }
+    }
+}
